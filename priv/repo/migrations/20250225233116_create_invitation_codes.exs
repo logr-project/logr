63f45ab0b67a1d24c792b6eddf11c0f628e989f5
Pikhosh <73311467+pikhosh@users.noreply.github.com>
2025-02-26 03:15:23 +0330

    🥤 More

--- priv/repo/migrations/20250225233116_create_invitation_codes.exs
+++ priv/repo/migrations/20250225233116_create_invitation_codes.exs
@@ -4,9 +4,14 @@ defmodule Logr.Repo.Migrations.CreateInvitationCodes do
   def change do
     create table(:invitation_codes, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :code, :string
-      add :profile_id, references(:profiles, on_delete: :nothing, type: :binary_id)
-      add :used_by_id, references(:profiles, on_delete: :nothing, type: :binary_id)
+      add :code, :string, size: 16, null: false
+
+      add :profile_id,
+          references(:profiles, on_update: :update_all, on_delete: :nothing, type: :binary_id),
+          null: false
+
+      add :used_by_id,
+          references(:profiles, on_update: :update_all, on_delete: :nothing, type: :binary_id)
 
       timestamps(type: :utc_datetime)
     end
@@ -14,5 +19,6 @@ defmodule Logr.Repo.Migrations.CreateInvitationCodes do
     create unique_index(:invitation_codes, [:used_by_id])
     create unique_index(:invitation_codes, [:code])
     create index(:invitation_codes, [:profile_id])
+    create constraint(:invitation_codes, :code_length, check: "OCTET_LENGTH == 16")
   end
 end
