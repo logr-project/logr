48ad25080a7052cca1e8fbec83050c13bce5831d
Pikhosh <73311467+pikhosh@users.noreply.github.com>
2025-02-28 17:51:19 +0330

    ☂️ Complete `logs` migration

--- priv/repo/migrations/20250228140153_create_logs.exs
+++ priv/repo/migrations/20250228140153_create_logs.exs
@@ -4,17 +4,30 @@ defmodule Logr.Repo.Migrations.CreateLogs do
   def change do
     create table(:logs, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :title, :string
-      add :slug, :string
-      add :quick_view, :text
-      add :content, :text
+      add :title, :string, size: 50, null: false
+      add :slug, :string, size: 50, null: false
+      add :quick_view, :text, null: false
+      add :content, :text, null: false
       add :logged_at, :utc_datetime
       add :discoverable, :boolean, default: false, null: false
-      add :space_id, references(:spaces, on_delete: :nothing, type: :binary_id)
+
+      add :space_id,
+          references(:spaces, on_update: :update_all, on_delete: :delete_all, type: :binary_id),
+          null: false
 
       timestamps(type: :utc_datetime)
     end
 
     create index(:logs, [:space_id])
+    create unique_index(:logs, [:space_id, :slug])
+    create constraint(:logs, :slug_contains, check: "OCTET_LENGTH(slug) >= 1")
+
+    create constraint(:logs, :quick_view_min_and_max,
+             check: "OCTET_LENGTH(quick_view) >= 3 AND OCTET_LENGTH(quick_view) <= 4096"
+           )
+
+    create constraint(:logs, :content_min_and_max,
+             check: "OCTET_LENGTH(content) >= 3 AND OCTET_LENGTH(content) <= 61440"
+           )
   end
 end
